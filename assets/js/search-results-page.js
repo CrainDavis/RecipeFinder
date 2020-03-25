61012f5f5a73a2d723764bb07575d1fb85b2707e
Juan Acosta <juandaniel_1@yahoo.com>
2020-03-24 19:05:06 -0700

    for loop added to the recipeSearch function

--- assets/js/search-results-page.js
+++ assets/js/search-results-page.js
@@ -34,54 +34,259 @@ function searchForRecipe(food) {
       "&apiKey=" +
       secondappKey;
 
-    // Basic information
-    // Recipe Name
-    var recipeName = response.hits[0].recipe.label;
-    $("#recipeName").empty();
-    $("#recipeName").append(recipeName);
+    for (var i = 0; i < response.hits.length; i++) {
+      // article id recipeResult
+      var articleRecipeResult = $("<article>");
+      articleRecipeResult.addClass(
+        "recipe-results-card tile is-child notification"
+      );
 
-    // Image from API
-    var searchImage = response.hits[0].recipe.image;
-    $(".search-image").attr("src", searchImage);
+      //appending it into div container seach-results
+      $("#test").append(articleRecipeResult);
+      //==============================================================
+      // p recipeName first child of article id recipeResult
+      var pRecipeName = $("<p>");
+      pRecipeName.addClass("recipe-name title");
+      pRecipeName.html(response.hits[i].recipe.label);
+      //appending it into article id RecipeResult
+      articleRecipeResult.append(pRecipeName);
+      //==============================================================
+      // div class content second child of article id
+      var divContent = $("<div>");
+      divContent.addClass("content");
+      //appending it into article id RecipeResult
+      articleRecipeResult.append(divContent);
 
-    // Calories
-    var resultCalories = response.hits[0].recipe.calories;
-    $(".result-calories").empty();
-    var resultCaloriesFixed = resultCalories.toFixed(0);
-    $(".result-calories").append(resultCaloriesFixed);
+      // article class recipe-item first child of divContent
+      var articleRecipeItem = $("<article>");
+      articleRecipeItem.addClass("recipe-item tile is-chld notification");
+      // appending it into divContent
+      divContent.append(articleRecipeItem);
 
-    // Serving Size
-    var servingNumber1 = response.hits[0].recipe.yield;
-    $(".result-servings").empty();
-    $(".result-servings").append(servingNumber1);
+      // div class content only child of articleRecipeItem
+      var divContentArticle = $("<div>");
+      divContentArticle.addClass("content");
+      //appending it into articleRecipeItem
+      articleRecipeItem.append(divContentArticle);
 
-    // Cooking Time
-    var prepTime1 = response.hits[0].recipe.totalTime;
-    $(".result-prep-time").empty();
-    $(".result-prep-time").append(prepTime1);
+      // div class columns only child of divContentArticle
+      // recipe cost, nutrition info, servings and time
+      var divColumns = $("<div>");
+      divColumns.addClass("columns");
+      divContentArticle.append(divColumns);
 
-    //Health & Diet
-    // var diet = response.hits[0].recipe.dietLabels;
+      // div class columns recipeStats only child of divColumns
+      var divColumnRecipeStats = $("<div>");
+      divColumnRecipeStats.addClass("column");
+      divColumnRecipeStats.attr("style", "text-align:center");
+      // appending it to divColumns
+      divColumns.append(divColumnRecipeStats);
 
-    var health = response.hits[0].recipe.healthLabels;
-    $(".description-health-info").empty();
-    $(".description-health-info").append(
-      "<ul><li>" + health.join("</li><li>") + "</li></ul>"
-    );
+      // div resultRecipeInfo Class result-recipe only child of divColumnRecipeStats
+      var divResultRecipe = $("<div>");
+      divResultRecipe.addClass("result-recipe-info columns");
+      //appending it into divColumnRecipeStats
+      divColumnRecipeStats.append(divResultRecipe);
 
-    //Ingredient List
-    var ingredientList = response.hits[0].recipe.ingredientLines;
-    // We need access to the array
-    $(".description-main").empty();
-    $(".description-main").append(
-      "<ul><li>" + ingredientList.join("</li><li>") + "</li></ul>"
-    );
+      // div class result-cost first child of divResultRecipe
+      var divResultCost = $("<div>");
+      divResultCost.addClass("result-cost column is-one-quarter");
+      //appending it into divResultRecipe
+      divResultRecipe.append(divResultCost);
 
-    // Get Cooking Link
+      // i cost and span cost children of divResultCost
+      var cost = $("<i>");
+      var spanCostTag = $("<span>");
+      spanCostTag.addClass("cost-number");
+      cost.addClass("fas fa-dollar-sign");
+      //appending them to divResultCost
+      divResultCost.append(cost, spanCostTag);
 
-    $("#externalLinkButton").on("click", function(event) {
+      // div class result-calories second child of divResultRecipe
+      var divResultCalories = $("<div>");
+      divResultCalories.addClass("column is-one-quarter");
+      //appending it into divResultRecipe
+      divResultRecipe.append(divResultCalories);
+
+      //  i calories and span calories children of divResultCalories
+      var iCaloriesTag = $("<i>");
+      var spanCaloriesTag = $("<span>");
+      spanCaloriesTag.addClass("result-calories");
+      iCaloriesTag.addClass("fas fa-fire");
+      spanCaloriesTag.html(response.hits[i].recipe.calories.toFixed(0));
+      //appending it into divResultCalories
+      divResultCalories.append(iCaloriesTag, spanCaloriesTag);
+
+      // div class result-servings third child of divResultRecipe
+      var divResultServings = $("<div>");
+      divResultServings.addClass("column is-one-quarter");
+      //appending it into divResultRecipe
+      divResultRecipe.append(divResultServings);
+
+      // i Serving Size and span servingsize children of divResultServing
+      var iServSizeTag = $("<i>");
+      var spanServSizeTag = $("<span>");
+      spanServSizeTag.addClass("result-servings");
+      iServSizeTag.addClass("fas fa-user");
+      spanServSizeTag.html(response.hits[i].recipe.yield);
+      // appending them into divResultServing
+      divResultServings.append(iServSizeTag, spanServSizeTag);
+
+      // div class result-prepTime fourth child of divResultRecipe
+      var divResultPrepTime = $("<div>");
+      divResultPrepTime.addClass("column is-one-quarter");
+      //appending it into divResultRecipe
+      divResultRecipe.append(divResultPrepTime);
+
+      // i cookingtime and span cookingtime children of divResultPrepTime
+      var iCookingTime = $("<i>");
+      var spanCookingTag = $("<span>");
+      spanCookingTag.addClass("result-prep-time");
+      iCookingTime.addClass("fas fa-clock");
+      spanCookingTag.html(response.hits[i].recipe.totalTime);
+      // appending them into divResultPrepTime
+      divResultPrepTime.append(iCookingTime, spanCookingTag);
+
+      //======================================================================
+      // div class tile is-ancenstor second child of divContent
+      var divIsAncestor = $("<div>");
+      divIsAncestor.addClass("tile is-ancestor");
+      // appending it into divContent
+      divContent.append(divIsAncestor);
+
+      // div tile is vertical is-12 only child of divIsAncestor
+      var divIsVertical = $("<div>");
+      divIsVertical.addClass("tile is-vertical is-12");
+      // appending it into divIsAncenstor
+      divIsAncestor.append(divIsVertical);
+
+      // div class tile only child of divIsVertical
+      var divTile = $("<div>");
+      divTile.addClass("tile");
+      //appending it into divIsVertical
+      divIsVertical.append(divTile);
+
+      // div class tile is-parent is-vertical child of divTile
+      var divTileIsParentVertical = $("<div>");
+      divTileIsParentVertical.addClass("tile is-parent is-vertical");
+      //appending it into divTile
+      divTile.append(divTileIsParentVertical);
+
+      // article class health indo tile first child of divTileisParentVertical
+      // health info and ingredients
+      var articleHealthInfo = $("<article>");
+      articleHealthInfo.addClass("health-info-tile tile is-child notification");
+      //appending it into divTileIsParentVertical
+      divTileIsParentVertical.append(articleHealthInfo);
+
+      // div first child of articleHealthInfo
+      var divfirstChild = $("<div>");
+      //appending it into articleHealthInfo
+      articleHealthInfo.append(divfirstChild);
+
+      // p class recipe-health-info only child of divFirstChild
+      var pRecipeHealth = $("<p>");
+      pRecipeHealth.addClass("subtitle recipe-health-info");
+      pRecipeHealth.attr("style", "font-weight:bold; color:#ef8499");
+      pRecipeHealth.html("Health Info");
+      var healthInfo = response.hits[i].recipe.healthLabels;
+
+      //appending it into divFirstChild
+      divfirstChild.append(pRecipeHealth);
+
+      // span description health info only child of pRecipeHealth
+      var spanDescriptionHealth = $("<span>");
+      spanDescriptionHealth.addClass("description-health-info");
+      spanDescriptionHealth.attr("style", "font-weight: normal; color:white;");
+      spanDescriptionHealth.append(
+        "<ul><li>" + healthInfo.join("</li><li>") + "</li></ul>"
+      );
+      // appending it into pRecipeHealth
+      pRecipeHealth.append(spanDescriptionHealth);
+
+      // div second child of articleHealthIndfo
+      var divSecondChild = $("<div>");
+      articleHealthInfo.append(divSecondChild);
+
+      // p class recipe-description-info only child of divSecondChild
+      var pRecipeDescription = $("<p>");
+      pRecipeDescription.addClass("subtitle recipe-health-info");
+      pRecipeDescription.attr(
+        "style",
+        "font-weight:bold; color:#ef8499; margin-top: 25px;"
+      );
+      pRecipeDescription.html("Ingredients:");
+      var recipeDescriptionInfo = response.hits[i].recipe.ingredientLines;
+
+      // appending it into the divSecondChild
+      divSecondChild.append(pRecipeDescription);
+
+      // span description main only child of pRecipeDescription
+      var spanRecipeDescription = $("<span>");
+      spanRecipeDescription.addClass("description-main");
+      spanRecipeDescription.attr("style", "font-weight: normal; color:white;");
+      spanRecipeDescription.append(
+        "<ul><li>" + recipeDescriptionInfo.join("</li><li>") + "</li></ul>"
+      );
+      // appending it into pRecipeDescription
+      pRecipeDescription.append(spanRecipeDescription);
+
+      // article buttonsTile second child of divTileIsParentVertical
+      // buttons
+      var articleButtons = $("<article>");
+      articleButtons.addClass("buttons-tile tile is-child notification");
+      divTileIsParentVertical.append(articleButtons);
+
+      // button element favorite-button first child of articleButtons
+      var favoriteButton = $("<button>");
+      favoriteButton.addClass("favorite-button is-large is danger");
+      favoriteButton.attr("style", "margin-right:50px");
+      favoriteButton.html("Favorite ");
+      articleButtons.append(favoriteButton);
+
+      // i class fas fa-heart
+      var heartIcon = $("<i>");
+      heartIcon.addClass("fas fa-heart");
+
+      // appending heartIcon into favoriteButton
+      favoriteButton.append(heartIcon);
+
+      // getCooking Button
+      var getCookingButton = $("<button>");
+      getCookingButton.addClass("external-link-button is-large is-danger");
+      getCookingButton.html("Get Cooking");
+      articleButtons.append(getCookingButton);
+
+      getCookingButton.on("click", function(event) {
+        event.preventDefault();
+        var getCookingHere = response.hits[i].recipe.url;
+        window.location = getCookingHere;
+      });
+
+      //===============================================================
+      // div class tile is parent second child of divTile
+      var divTileisParent = $("<div>");
+      divTileisParent.addClass("tile-is-parent");
+      //Appending it into divTile
+      divTile.append(divTileisParent);
+
+      //article class image-tile only child of divTileisParent
+      var articleImageTile = $("<article>");
+      articleImageTile.addClass("image-tile tile is-child notification");
+      // appending it to divTileisParent
+      divTileisParent.append(articleImageTile);
+
+      //image class search-image
+      var imageSearch = $("<img>");
+      imageSearch.addClass("search-image");
+      imageSearch.attr("style", "margin-left:20px");
+      imageSearch.attr("src", response.hits[i].recipe.image);
+      articleImageTile.append(imageSearch);
+    }
+
+    $(".external-link-button").on("click", function(event) {
       event.preventDefault();
-      var getCooking = response.hits[0].recipe.url;
+      var getCooking = response.hits[i].recipe.url;
 
       window.location = getCooking;
     });
@@ -90,9 +295,9 @@ function searchForRecipe(food) {
       url: queryURLsecondApi,
       method: "GET"
     }).then(function(response) {
-      console.log(response.results[0]);
+      console.log(response.results[i]);
 
-      var priceId = response.results[0].id;
+      var priceId = response.results[i].id;
 
       var priceBreakDown =
         "https://api.spoonacular.com/recipes/" +
